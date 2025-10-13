edd66a87633d460cde3b5f9e060997bbf75eb508
Svachuta Gollavilli <sunil202pydah@gmail.com>
2025-10-13 19:51:24 -0400

    checking the folder Hierarchy

---
+++ ChatModels/chatmodel_demo.py
@@ -0,0 +1,13 @@
+from langchain import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI(model = "gpt-4", temperature = 0.8, max_completion_tokens = 50)
+
+result = model.invoke(
+    "tell me a philosophical telugu shayari on heart"
+)
+
+
+print(result.content)
